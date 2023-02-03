b4abc70ddb2515568f2dfc41980a7decb2c01eb8
RyanBruton <lebbern2@hotmail.co.uk>
2023-02-03 19:14:53 +0000

    deploy test

--- src/App.js
+++ src/App.js
@@ -2,16 +2,19 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Ryan Bruton
-        </p>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    Ryan Bruton
+                </p>
+                <p>
+                    Biz is a rat
+                </p>
+            </header>
+        </div>
+    );
 }
 
 export default App;
