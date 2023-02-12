c7b0793975617cd24146fad980bdaf07862597af
RyanBruton <lebbern2@hotmail.co.uk>
2023-02-12 15:39:46 +0000

    Added socials to hero

--- src/Components/Hero/Hero.js
+++ src/Components/Hero/Hero.js
@@ -1,5 +1,7 @@
 import './Hero.css';
 import heroImage from '../../Assets/SeatedPortrait.png';
+import { FaGithub, FaYoutube, FaTwitter, FaLinkedin } from 'react-icons/fa';
+import { IconContext } from "react-icons";
 
 const Hero = () => {
     return (
@@ -14,6 +16,22 @@ const Hero = () => {
                     <p>Can be found riding around on an eboard****</p>
                     <p>Generally has too many interests*****</p>
                 </div>
+                <IconContext.Provider value={{ color: "white", size: "2em", className: "social" }}>
+                    <div className="hero__content-socials">
+                        <a href="https://github.com/stinkidog" target="_blank" rel="noreferrer">
+                            <FaGithub />
+                        </a>
+                        <a href="https://www.youtube.com/channel/UClCdnDtnhsKTX0nstHuEC3A" target="_blank" rel="noreferrer">
+                            <FaYoutube />
+                        </a>
+                        <a href="https://twitter.com/TheRyanBruton" target="_blank" rel="noreferrer">
+                            <FaTwitter />
+                        </a>
+                        <a href="https://www.linkedin.com/in/theryanbruton/" target="_blank" rel="noreferrer">
+                            <FaLinkedin />
+                        </a>
+                    </div>
+                </IconContext.Provider>
             </div>
             <div className="hero__image-container">
                 <img className="hero__image" src={heroImage} />
--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -18,7 +18,6 @@ const NavBar = () => {
             };
 
             if (!e.target.classList.contains('active-link')) {
-                console.log("trueee");
                 e.target.classList.add('active-link');
             }
         }
@@ -37,12 +36,12 @@ const NavBar = () => {
                     ? <RiCloseLine color="#fff" size={30} onClick={() => setToggleMenu(false)} />
                     : <RiMenu3Line color="#fff" size={30} onClick={() => setToggleMenu(true)} />}
                 {toggleMenu && (
-                    <div className="header__navbar-menu_container scale-up-center">
+                    <div className="header__navbar-menu_container">
                         <div className="header__navbar-menu_container-links">
-                            <p><a href="#home">Home</a></p>
-                            <p><a href="#about">About</a></p>
-                            <p><a href="#blog">Blog</a></p>
-                            <p><a href="#contact">Contact</a></p>
+                            <p><a className="link active-link" onClick={toggleActive} href="#home">Home</a></p>
+                            <p><a className="link" onClick={toggleActive} href="#about">About</a></p>
+                            <p><a className="link" onClick={toggleActive} href="#blog">Blog</a></p>
+                            <p><a className="link" onClick={toggleActive} href="#contact">Contact</a></p>
                         </div>
                     </div>
                 )}
