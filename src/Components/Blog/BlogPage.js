567e4104f448bdb907b0c3296c1230f00c50782a
RyanBruton <lebbern2@hotmail.co.uk>
2023-03-25 10:24:26 +0000

    Blog to be reverse style of home hero with content underneath

--- src/Components/Blog/BlogPage.js
+++ src/Components/Blog/BlogPage.js
@@ -4,14 +4,20 @@ import './BlogPage.css';
 const BlogPage = (props) => {
     return(
         <div className="blogPage section__padding">
-            <div className="blogPage__image-container">
-                <img className="blogPage__image" src={`../BlogImages/${props.blogData.image}`} />
+            <div className="blogPage__hero">
+                <div className="blogPage__hero-image">
+                    <img className="blogPage__image" src={`../BlogImages/${props.blogData.image}`} />
+                </div> 
+                <div className="blogPage__hero-content">
+                    <h1>{props.blogData.title}</h1>
+                    <h2>{props.blogData.category}</h2>
+                </div>
             </div>
-            <div className="blogPage__content">
+            {/* <div className="blogPage__content">
                 <h1>{props.blogData.title}</h1>
                 <h4>{props.blogData.category}</h4>
                 <div dangerouslySetInnerHTML={{__html: props.blogData.html}} />
-            </div>
+            </div> */}
         </div>
     );
 }
--- src/Containers/Blog/Blog.js
+++ src/Containers/Blog/Blog.js
@@ -10,6 +10,7 @@ const Blog = () => {
 
     const { id } = useParams();
     const [blogId, setBlogId] = useState(id);
+    const [blogContent, setBlogContent] = useState({});
     const blogRef = useRef();
 
     const blogNavLinks = [
@@ -20,27 +21,16 @@ const Blog = () => {
         }
     ];
 
-    const mockData = {
-        "id": 0,
-        "title": "Coming Soon",
-        "category": "Health and Lifestyle",
-        "image": "MeAndGoggins.JPG",
-        "html": "<h1>HTML Ipsum Presents</h1>\n\n\t\t\t\t<p><strong>Pellentesque habitant morbi tristique</strong> senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. <em>Aenean ultricies mi vitae est.</em> Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, <code>commodo vitae</code>, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. <a href=\"#\">Donec non enim</a> in turpis pulvinar facilisis. Ut felis.</p>\n\n\t\t\t\t<h2>Header Level 2</h2>\n\n\t\t\t\t<ol>\n\t\t\t\t   <li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</li>\n\t\t\t\t   <li>Aliquam tincidunt mauris eu risus.</li>\n\t\t\t\t</ol>\n\n\t\t\t\t<blockquote><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus magna. Cras in mi at felis aliquet congue. Ut a est eget ligula molestie gravida. Curabitur massa. Donec eleifend, libero at sagittis mollis, tellus est malesuada tellus, at luctus turpis elit sit amet quam. Vivamus pretium ornare est.</p></blockquote>\n\n\t\t\t\t<h3>Header Level 3</h3>\n\n\t\t\t\t<ul>\n\t\t\t\t   <li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</li>\n\t\t\t\t   <li>Aliquam tincidunt mauris eu risus.</li>\n\t\t\t\t</ul>\n\n\t\t\t\t<pre><code>\n\t\t\t\t#header h1 a {\n\t\t\t\t  display: block;\n\t\t\t\t  width: 300px;\n\t\t\t\t  height: 80px;\n\t\t\t\t}\n\t\t\t\t</code></pre>"
-    }
-
     useEffect(() => {
-
-        //Get the ID from url params from json object to pass into BlogPage component below
-        //Can also use this if adding any links to other blogs to update on the page
-        console.log(BlogContent);
-        console.log(blogId);
+        var blogData = BlogContent.posts.find(x => x.id === parseInt(blogId));
+        setBlogContent(blogData);
     });
 
     return(
         <div className="App">
             <div id="blog" ref={blogRef} className="gradient__bg">
                 <Header links={blogNavLinks} />
-                <BlogPage blogData={mockData} />
+                <BlogPage blogData={blogContent} />
                 <Footer />
             </div>
         </div>
